72459b026626dde1a45b4666f89efa8d681c1dad
SpiritTeaHouse <2480040021@qq.com>
2024-07-08 14:41:44 +0800

    第三方bean导入项目初始化

--- springboot-thirdbean/src/main/java/com/itheima/SpringbootThirdbeanApplication.java
+++ springboot-thirdbean/src/main/java/com/itheima/SpringbootThirdbeanApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebTliasApplication {
+public class SpringbootThirdbeanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebTliasApplication.class, args);
+        SpringApplication.run(SpringbootThirdbeanApplication.class, args);
     }
+
 }
